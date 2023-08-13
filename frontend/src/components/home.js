56dc2f28f571f662abc7aa571d5ced524dfe6354
Jake Korobellis <jkorobellis@gmail.com>
2023-08-12 21:08:30 -0400

    Stlying & has account

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -8,6 +8,18 @@ function Home() {
         The latest in <span className="white large-sub-head">Equities</span> &{" "}
         <span className="white large-sub-head">Crypto</span>{" "}
       </span>
+
+      <div className="has-account">
+        <div>Do you have an account?</div>
+        <div className="buttons-home">
+          <a href="/has-account/true" className="home-button-link-yes">
+            Yes
+          </a>
+          <a href="/has-account/true" className="home-button-link-no">
+            No
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
