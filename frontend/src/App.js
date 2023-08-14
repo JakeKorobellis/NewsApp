685225cad5b177e264913fa09dfc675edd701755
Jake Korobellis <jkorobellis@gmail.com>
2023-08-13 22:08:30 -0400

    Test Route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import ConetentHome from "./components/content-home";
+import Test from "./components/test";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route index element={<Home />} />
-            <Route path="/content" element={<ConetentHome />} />
+            <Route path="/content" element={<ConetentHome />}></Route>
           </Routes>
         </BrowserRouter>
       </header>
---
+++ frontend/src/components/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Test() {
+  return (
+    <div className="home">
+      <h1>Connected</h1>
+    </div>
+  );
+}
+
+export default Test;
