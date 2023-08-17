97333cc52759a9540e611cb320b03820c1225948
Jake Korobellis <jkorobellis@gmail.com>
2023-08-16 22:13:55 -0400

    API Limit Reached

--- frontend/src/components/split.js
+++ frontend/src/components/split.js
@@ -8,6 +8,8 @@ function Split() {
   const api_fi = process.env.REACT_APP_API_FI;
   const crypto_first = process.env.REACT_APP_CRYPTO_FIRST;
   const crypto_second = process.env.REACT_APP_CRYPTO_SECOND;
+  const fi_first = process.env.REACT_APP_FI_FRONT;
+  const fi_second = process.env.REACT_APP_FI_BACK;
 
   //States
   const [news, setNews] = React.useState([]); //Crypto news State
@@ -25,6 +27,7 @@ function Split() {
       .then((res) => setFi(res.feed));
   }, []);
 
+  console.log(fi);
   //Reusable crypto render
   function reuse_crpyto(news) {
     return news.map((curr) => {
@@ -73,6 +76,19 @@ function Split() {
     console.log(choice);
   }
 
+  //Reusable selection - Fi
+  function fi_selection(choice) {
+    if (choice === "latest") {
+      fetch(api_fi) //Securities API -- SPY/QQQ
+        .then((res) => res.json())
+        .then((res) => setFi(res.feed));
+    } else {
+      fetch(fi_first + choice + fi_second)
+        .then((res) => res.json())
+        .then((res) => setFi(res.feed));
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -96,14 +112,23 @@ function Split() {
             <div className="title-all">Split</div>
             <div className="selection-split-hold">
               <div className="fifty-selection">{curr_crypto}</div>
-              <div className="fifty-selection">{curr_fi}</div>
+
+              {fi ? (
+                <div className="fifty-selection">{curr_fi}</div>
+              ) : (
+                <div></div>
+              )}
             </div>
             <div className="stream-hold-all wider-hold-all">
               <div className="crypto-hold">
                 {news !== [] ? reuse_crpyto(news) : "Loading"}
               </div>
               <div className="stocks-hold">
-                {fi !== [] ? reuse_finance(fi) : "Loading"}
+                {fi ? (
+                  reuse_finance(fi)
+                ) : (
+                  <div className="limit-reach">Equities API Limit Reached</div>
+                )}
               </div>
             </div>
             <div className="split-button-hold">
@@ -155,7 +180,58 @@ function Split() {
                   Bearish
                 </button>
               </div>
-              <div className="splity-fifty"></div>
+              {fi ? (
+                <div className="splity-fifty">
+                  <button
+                    onClick={() => {
+                      crypto_selection("latest");
+                      set_curr_fi("Latest");
+                    }}
+                    className="button-split"
+                  >
+                    Latest
+                  </button>
+                  <button
+                    onClick={() => {
+                      fi_selection("earnings");
+                      set_curr_fi("Earnings");
+                    }}
+                    className="button-split"
+                  >
+                    Earnings
+                  </button>
+                  <button
+                    onClick={() => {
+                      fi_selection("ipo");
+                      set_curr_fi("IPO");
+                    }}
+                    className="button-split"
+                  >
+                    IPO
+                  </button>
+
+                  <button
+                    onClick={() => {
+                      fi_selection("mergers_and_acquisitions");
+                      set_curr_fi("M&A");
+                    }}
+                    className="button-split"
+                  >
+                    M&A
+                  </button>
+                  <button
+                    onClick={() => {
+                      fi_selection("technology");
+                      set_curr_fi("Tech");
+                    }}
+                    className="button-split"
+                  >
+                    Tech
+                  </button>
+                </div>
+              ) : (
+                <div></div>
+              )}
             </div>
           </div>
         </div>
