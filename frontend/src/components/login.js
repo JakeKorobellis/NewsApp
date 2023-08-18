22a35c3eb66d98a2daa0042f80066b7234e7d505
Jake Korobellis <jkorobellis@gmail.com>
2023-08-17 21:33:00 -0400

    Login page complete, temp bypass included

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -9,8 +9,37 @@ function Login() {
       </div>
 
       <div className="login-page-split">
-        Login Form
-        <a href="/content">Login</a>
+        <div className="form">
+          <form action="/login" method="post" className="form">
+            <div className="form-inputs">
+              <label className="resize">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="jdoe@newsroom.com"
+                className="inputs-form"
+                required
+              />
+            </div>
+
+            <div className="form-inputs">
+              <label for="password" className="resize">
+                Password:
+              </label>
+
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="********"
+                className="inputs-form"
+                required
+              />
+            </div>
+            <input type="submit" value="Login" className="login-btn" />
+          </form>
+        </div>
       </div>
       <div className="footer-login-page black">
         Dont have an account?{" "}
@@ -18,6 +47,9 @@ function Login() {
           Sign up!
         </a>
       </div>
+      <a href="/content" className="supersmall">
+        Temp Bypass
+      </a>
     </div>
   );
 }
