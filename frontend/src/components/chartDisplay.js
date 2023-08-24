e36efb34f4e345e695fa0995f67fc7a0d11903e9
Jake Korobellis <jkorobellis@gmail.com>
2023-08-23 23:21:27 -0400

    Updated Chart Styling

--- frontend/src/components/chartDisplay.js
+++ frontend/src/components/chartDisplay.js
@@ -17,40 +17,40 @@ export default function Chart({ priceData }) {
       width: chartWrapperRef.current.offsetWidth, // Use wrapper's width
       height: chartWrapperRef.current.offsetHeight, // Use wrapper's height
       layout: {
-        backgroundColor: "#253248",
+        backgroundColor: "black",
         textColor: "rgba(255, 255, 255, 0.9)",
       },
       crosshair: {
         mode: CrosshairMode.Normal,
       },
       layout: {
-        background: { color: "#000000" },
-        textColor: "#ffffff",
+        background: { color: "black" },
+        textColor: "white",
       },
       grid: {
         vertLines: {
-          color: "#404040",
+          color: "black",
         },
         horzLines: {
-          color: "#404040",
+          color: "black",
         },
       },
       priceScale: {
         borderColor: "#cccccc",
       },
       timeScale: {
-        borderColor: "#cccccc",
+        borderColor: "#2272FF",
         timeVisible: true,
       },
     });
 
     const candleSeries = chart.addCandlestickSeries({
-      upColor: "#4bffb5",
-      downColor: "#ff4976",
-      borderDownColor: "#ff4976",
-      borderUpColor: "#4bffb5",
-      wickDownColor: "#838ca1",
-      wickUpColor: "#838ca1",
+      upColor: "#2272FF",
+      downColor: "white",
+      borderDownColor: "white",
+      borderUpColor: "#2272FF",
+      wickDownColor: "white",
+      wickUpColor: "#2272FF",
     });
 
     candleSeries.setData(priceData);
