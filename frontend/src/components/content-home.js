8bd516841a14dd4d8d618b01c34be8228740d09c
Jake Korobellis <jkorobellis@gmail.com>
2023-08-26 12:17:07 -0400

    Recent news to populate before stream starts

--- frontend/src/components/content-home.js
+++ frontend/src/components/content-home.js
@@ -1,10 +1,31 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import React from "react";
 import Side from "./sidebar";
-import News from "./get_news_latest";
 
 function ConetentHome() {
-  News();
+  const key = process.env.REACT_APP_KEY;
+  const secret = process.env.REACT_APP_SECRET;
+  const url = process.env.REACT_APP_URL14;
+  const [prev, setPrev] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(url, {
+      headers: {
+        "APCA-API-KEY-ID": key,
+        "APCA-API-SECRET-KEY": secret,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        // Process the response data here
+        setPrev(response.news);
+      })
+      .catch((error) => {
+        // Handle errors here
+        console.error("Error:", error);
+      });
+  }, []);
+
+  console.log(prev);
 
   return (
     <div className="App">
