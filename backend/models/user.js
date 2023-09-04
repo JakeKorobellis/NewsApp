678525eac49c8d6d317384c242634738c72116f3
Jake Korobellis <jkorobellis@gmail.com>
2023-09-04 12:29:31 -0400

    User Schema

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = require("mongodb");
+
+//User model
+const UserSchema = new Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  fav_news: { type: ObjectId, required: false },
+});
