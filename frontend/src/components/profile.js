ea4a74ee070c42e3211251a4d2e23b759977248c
Jake Korobellis <jkorobellis@gmail.com>
2023-09-13 20:03:02 -0400

    Populating dyanmic user edit fields

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -3,12 +3,30 @@ import Side from "./sidebar";
 import user from "./pictures/user.png";
 
 function UserEdit() {
+  const [userData, setUserData] = React.useState([]);
+
   const [currUser, setCurrUser] = React.useState({
     fname: "John",
     lname: "Doe",
     email: "jdoe@newsroom.com",
     password: "JohnDoe123",
   });
+
+  const token = localStorage.getItem("token");
+
+  React.useEffect(() => {
+    fetch("/api/auth", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setUserData(data.authData.user));
+  }, []);
+
+  console.log(userData);
 
   //Use effect to set the current user when loaded
 
@@ -38,68 +56,79 @@ function UserEdit() {
           <div class="sidebar">
             <Side />
           </div>
-          <div class="data stream-all">
-            <div className="title-all">Edit Profile</div>
-            <div>Current profile values are populated.</div>
-            <div className="stream-hold-all10">
-              <div className="signup-page-split">
-                <div className="form">
-                  <form
-                    action="/useredit/update"
-                    method="post"
-                    className="form"
-                  >
-                    <div className="form-inputs">
-                      <label className="resize">First Name:</label>
+          {userData == [] ? (
+            <div className="holder-loader">
+              <div class="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          ) : (
+            <div class="data stream-all">
+              <div className="title-all">Edit Profile</div>
+              <div>Current profile values are populated.</div>
+              <div className="stream-hold-all10">
+                <div className="signup-page-split">
+                  <div className="form">
+                    <form
+                      action="/useredit/update"
+                      method="post"
+                      className="form"
+                    >
+                      <div className="form-inputs">
+                        <label className="resize">First Name:</label>
+                        <input
+                          type="fname"
+                          id="fname"
+                          name="fname"
+                          className="inputs-form"
+                          value={userData.fname}
+                          required
+                        />
+                      </div>
+                      <div className="form-inputs">
+                        <label className="resize">Last Name:</label>
+                        <input
+                          type="lname"
+                          id="lname"
+                          name="lname"
+                          className="inputs-form"
+                          value={userData.lname}
+                          required
+                        />
+                      </div>
+                      <div className="form-inputs">
+                        <label className="resize">Email:</label>
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="inputs-form"
+                          value={userData.email}
+                          required
+                        />
+                      </div>
+                      <a href="/useredit/password" className="login-btn center">
+                        Update password
+                      </a>
+                    </form>
+                  </div>
+                  <div className="form margin-top">
+                    {/* a href needs to be removed for backend and input needs to be moved into the form for the POST request to go off*/}
+                    <a href="/success" className="form">
                       <input
-                        type="fname"
-                        id="fname"
-                        name="fname"
-                        className="inputs-form"
-                        value={currUser.fname}
-                        required
+                        type="submit"
+                        value="Submit Profile Changes"
+                        className="login-btn"
                       />
-                    </div>
-                    <div className="form-inputs">
-                      <label className="resize">Last Name:</label>
-                      <input
-                        type="lname"
-                        id="lname"
-                        name="lname"
-                        className="inputs-form"
-                        value={currUser.lname}
-                        required
-                      />
-                    </div>
-                    <div className="form-inputs">
-                      <label className="resize">Email:</label>
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="inputs-form"
-                        value={currUser.email}
-                        required
-                      />
-                    </div>
-                    <a href="/useredit/password" className="login-btn center">
-                      Update password
                     </a>
-                  </form>
-                </div>
-                <div className="form margin-top">
-                  {/* a href needs to be removed for backend and input needs to be moved into the form for the POST request to go off*/}
-                  <a href="/success" className="form">
-                    <input
-                      type="submit"
-                      value="Submit Profile Changes"
-                      className="login-btn"
-                    />
-                  </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </header>
     </div>
