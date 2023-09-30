bf73137b88d4df6d475a405cae52e36b554f9643
Jake Korobellis <jkorobellis@gmail.com>
2023-09-29 23:06:33 -0400

    Data testing with faviorties

--- frontend/src/components/favs.js
+++ frontend/src/components/favs.js
@@ -43,7 +43,7 @@ function Fav() {
                 className="faviortie-btn"
                 onClick={() => console.log("Remove!")}
               >
-                remove
+                Remove
               </button>
             </div>
           </div>
