a618eea116f626944f02af99d98038e4d62ed7ea
Jake Korobellis <jkorobellis@gmail.com>
2023-10-02 21:16:53 -0400

    Save feature on search and tests complete

--- frontend/src/components/helperfunctions/formatData.js
+++ frontend/src/components/helperfunctions/formatData.js
@@ -1,7 +1,8 @@
 import convertISOToFormattedDateTime from "../time_convert-2";
+import Fav from "./favs";
 
 //For the search faeture
-export default function formatData(news) {
+export default function formatData(news, callback, user) {
   return news.map((curr) => {
     return (
       <div className="hold-crypto-news">
@@ -16,6 +17,21 @@ export default function formatData(news) {
           <div className="small-text-date">
             {convertISOToFormattedDateTime(curr.updated_at)}
           </div>
+          <button
+            className="faviortie-btn"
+            onClick={() =>
+              Fav(
+                curr.headline,
+                curr.source,
+                curr.url,
+                convertISOToFormattedDateTime(curr.updated_at),
+                callback,
+                user
+              )
+            }
+          >
+            Save
+          </button>
         </div>
       </div>
     );
