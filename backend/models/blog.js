af887deb91278e89a233bd2520640ca3f3dc1ff3
Jake Korobellis <jkorobellis@gmail.com>
2023-11-15 19:51:25 -0500

    Updated reference to users collection when blogs are sent to backend

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const { objectId, Timestamp } = require("mongodb");
 
 const BlogSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "users", required: true },
   title: { type: String, required: true },
   post: { type: String, required: true },
   date: { type: Date, default: Date.now },
