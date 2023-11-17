9480c180bdec04701696f67a4c5cffb7489cc45c
Jake Korobellis <jkorobellis@gmail.com>
2023-11-17 00:40:06 -0500

    New feature added

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import user from "./pictures/user.png";
+import Marquee from "react-fast-marquee";
 
 function Header() {
   /**
@@ -17,6 +18,22 @@ function Header() {
             </div>
           </a>
         </div>
+      </div>
+      <div>
+        <Marquee
+          style={{
+            width: "60vw",
+            height: "10vh",
+          }}
+          gradient={false}
+          speed={20}
+        >
+          <div className="rotate-title font-marquee">In the market:</div>
+          <div className="rotate-title-dece font-marquee">News1 </div>
+          <div className="rotate-title-dece font-marquee">News2 </div>
+          <div className="rotate-title-dece font-marquee">news3: </div>
+          <div className="rotate-title-dece font-marquee">news4: </div>
+        </Marquee>
       </div>
       <div className="fifityvw2">
         {" "}
