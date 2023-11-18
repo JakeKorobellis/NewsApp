7e996a3bd76f5fdc51240c727c48d1035a871318
Jake Korobellis <jkorobellis@gmail.com>
2023-11-18 18:12:47 -0500

    Restoried

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,23 @@
-import express, { json } from "express";
-import { set, connect } from "mongoose";
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
-import router from "./routes/index";
+const router = require("./routes/index");
 var app = express();
 
-app.use(json());
+app.use(express.json());
 app.use("/", router);
 
 app.get("/api", (req, res) => {
   res.json({ test: true });
 });
 
-set("strictQuery", false);
+mongoose.set("strictQuery", false);
 
 const DB = process.env.DBLINK;
 
 main().catch((err) => console.log(err));
 async function main() {
-  await connect(DB);
+  await mongoose.connect(DB);
   console.log("MongoDB is connected");
 }
 app.listen(5000, () => console.log("App is listening on port 5000"));
